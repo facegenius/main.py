57c33368d6fb695ec08dbac753fc9ebf59180250
facegenius <86742292+facegenius@users.noreply.github.com>
2025-04-11 16:50:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu", 
+            }
 
+for i in range (5):
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+        print("")
+    else:
+        print("kata tidak ditemukan")
+        print("")
